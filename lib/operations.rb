63785d60d26aed358b905f69cad550a3f790a52e
Martin Moy <martinmoy1991@gmail.com>
2020-10-03 09:55:39 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,10 @@
+require 'pry'
+puts a = "2"
+
 def unsafe?(speed)
 	if speed < 40 || speed > 60
 		return true
+		binding.pry
 	else
 		return false
 	end
@@ -8,4 +12,5 @@ end
 
 def not_safe?(speed)
 	speed < 40 || speed > 60 ? true:false
+
 end
